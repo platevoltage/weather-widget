9b1f1c980137f655f128f19d48b531d20a521e6f
antieatingactivist <antieatingactivist@gmail.com>
2022-01-07 22:35:18 -0800

    implemented save previous searches

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,9 @@
 const dateEl = document.getElementById('date');
 const forecastEl = document.getElementById('forecast');
+const previousSearchEl = document.getElementById('previous-search');
+const searchEl = document.getElementById('search');
 // console.log(header);
-
+var previousSearchArray = ["San Francisco, CA","Richmond, VA","Albuquerque, NM"];
 var date = moment();
 
 dateEl.innerHTML = date.format('dddd, MMMM Do YYYY');
@@ -10,3 +12,27 @@ forecastEl.children[1].innerHTML = date.add(2,'days').format('dddd');
 forecastEl.children[2].innerHTML = date.add(1,'days').format('dddd');
 forecastEl.children[3].innerHTML = date.add(1,'days').format('dddd');
 forecastEl.children[4].innerHTML = date.add(1,'days').format('dddd');
+
+updatePreviousSearch();
+
+function updatePreviousSearch() {
+    for (var i = 0; i < previousSearchArray.length; i++) {
+        let newSave = document.createElement("button");
+        newSave.innerHTML = previousSearchArray[i];
+        previousSearchEl.appendChild(newSave);
+    }
+}
+
+searchEl.addEventListener('submit', function(event) {
+
+    let search = document.getElementById("user-search");
+    let newSave = document.createElement("button");
+    
+    event.preventDefault();
+    // console.log(search.value);
+    previousSearchArray.unshift(search.value);
+    newSave.innerHTML = search.value;
+    previousSearchEl.insertBefore(newSave, previousSearchEl.firstChild);
+    search.value = "";
+
+  });
