368eb4f455cda73a890d124dbfc865fa796e18ff
antieatingactivist <antieatingactivist@gmail.com>
2022-01-07 21:56:33 -0800

    added dynamic day labels to forecast

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,12 @@
 const dateEl = document.getElementById('date');
+const forecastEl = document.getElementById('forecast');
 // console.log(header);
 
-var date = moment().format('dddd, MMMM Do YYYY');
+var date = moment();
 
-dateEl.innerHTML = date;
+dateEl.innerHTML = date.format('dddd, MMMM Do YYYY');
 
+forecastEl.children[1].innerHTML = date.add(2,'days').format('dddd');
+forecastEl.children[2].innerHTML = date.add(1,'days').format('dddd');
+forecastEl.children[3].innerHTML = date.add(1,'days').format('dddd');
+forecastEl.children[4].innerHTML = date.add(1,'days').format('dddd');
