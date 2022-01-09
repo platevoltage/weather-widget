1fc79e9536d6d81f8656980c9283fc1f57b3f0f1
antieatingactivist <antieatingactivist@gmail.com>
2022-01-09 00:57:50 -0800

    added clear history button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,17 +2,78 @@ const dateEl = document.getElementById('date');
 const forecastEl = document.getElementById('forecast');
 const previousSearchEl = document.getElementById('previous-search');
 const searchEl = document.getElementById('search');
+const clearHistoryEl = document.getElementById('clear-history');
 const cityEl = document.getElementById('city');
+const coordinatesEl = document.getElementById('coordinates');
+const weatherEl = document.getElementById('weather');
+const weatherIconEl = document.getElementById('weather-icon');
 const temperatureEl = document.getElementById('temperature');
 const humidityEl = document.getElementById('humidity');
 const windSpeedEl = document.getElementById('wind-speed');
 const uvIndexEl = document.getElementById('uv-index');
-const uvIndexContainerEl = document.getElementById('uv-index-container');
 
 
-const icons = {
+
+const iconsDay = {
     800 : "<i class='bi bi-brightness-high'></i>",
     801 : "<i class='bi bi-cloud-sun'></i>",
+    802 : "<i class='bi bi-cloud'></i>",
+    803 : "<i class='bi bi-clouds'></i>",
+    804 : "<i class='bi bi-clouds'></i>",
+    500 : "<i class='bi bi-cloud-drizzle'></i>",
+    501 : "<i class='bi bi-cloud-drizzle'></i>",
+    502 : "<i class='bi bi-cloud-rain-heavy'></i>",
+    503 : "<i class='bi bi-cloud-rain-heavy'></i>",
+    504 : "<i class='bi bi-cloud-rain-heavy'></i>",
+    511 : "<i class='bi bi-cloud-sleet'></i>",
+    520 : "<i class='bi bi-cloud-drizzle'></i>",
+    521 : "<i class='bi bi-cloud-drizzle'></i>",
+    522 : "<i class='bi bi-cloud-rain-heavy'></i>",
+    531 : "<i class='bi bi-cloud-rain-heavy'></i>",
+    300 : "<i class='bi bi-cloud-drizzle'></i>",
+    301 : "<i class='bi bi-cloud-drizzle'></i>",
+    302 : "<i class='bi bi-cloud-drizzle'></i>",
+    310 : "<i class='bi bi-cloud-drizzle'></i>",
+    311 : "<i class='bi bi-cloud-drizzle'></i>",
+    312 : "<i class='bi bi-cloud-drizzle'></i>",
+    313 : "<i class='bi bi-cloud-drizzle'></i>",
+    314 : "<i class='bi bi-cloud-drizzle'></i>",
+    321 : "<i class='bi bi-cloud-drizzle'></i>",
+    200 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    201 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    202 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    210 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    211 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    212 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    221 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    230 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    231 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    232 : "<i class='bi bi-cloud-lightning-rain'></i>",
+    701 : "<i class='bi bi-cloud-fog2'></i>",
+    711 : "<i class='bi bi-cloud-fog2'></i>",
+    721 : "<i class='bi bi-cloud-fog2'></i>",
+    731 : "<i class='bi bi-cloud-fog2'></i>",
+    741 : "<i class='bi bi-cloud-fog2'></i>",
+    751 : "<i class='bi bi-cloud-fog2'></i>",
+    761 : "<i class='bi bi-cloud-fog2'></i>",
+    762 : "<i class='bi bi-cloud-fog2'></i>",
+    771 : "<i class='bi bi-cloud-fog2'></i>",
+    781 : "<i class='bi bi-cloud-fog2'></i>",
+    600 : "<i class='bi bi-cloud-snow'></i>",
+    601 : "<i class='bi bi-cloud-snow'></i>",
+    602 : "<i class='bi bi-cloud-snow'></i>",
+    611 : "<i class='bi bi-cloud-snow'></i>",
+    612 : "<i class='bi bi-cloud-snow'></i>",
+    613 : "<i class='bi bi-cloud-snow'></i>",
+    615 : "<i class='bi bi-cloud-snow'></i>",
+    616 : "<i class='bi bi-cloud-snow'></i>",
+    620 : "<i class='bi bi-cloud-snow'></i>",
+    621 : "<i class='bi bi-cloud-snow'></i>",
+    622 : "<i class='bi bi-cloud-snow'></i>",
+};
+const iconsNight = {
+    800 : "<i class='bi bi-moon-stars'></i>",
+    801 : "<i class='bi bi-cloud-moon'></i>",
     802 : "<i class='bi bi-cloud'></i>",
     803 : "<i class='bi bi-clouds'></i>",
     804 : "<i class='bi bi-clouds'></i>",
@@ -81,6 +142,11 @@ if (localStorage.getItem("previousSearch")) {
 }
 
 dateEl.innerHTML = date.format('dddd, MMMM Do YYYY');
+forecastEl.children[0].children[0].children[0].innerHTML = "Tomorrow";
+forecastEl.children[1].children[0].children[0].innerHTML = date.add(2,'days').format('dddd');
+forecastEl.children[2].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
+forecastEl.children[3].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
+forecastEl.children[4].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
 
 
 if (previousSearchArray.length > 0) {
@@ -117,7 +183,7 @@ searchEl.addEventListener('submit', function(event) {
 
         }
         weatherFetch(weatherURL + search.value + apiKey);
-        localStorage.setItem("previousSearch", JSON.stringify(previousSearchArray) );
+        
         
         
         search.value = "";
@@ -128,7 +194,11 @@ searchEl.addEventListener('submit', function(event) {
   previousSearchEl.addEventListener("click", function(event) {
     weatherFetch(weatherURL + event.target.textContent + apiKey);
   });
-
+  clearHistoryEl.addEventListener("click", function(event) {
+    previousSearchArray = [];
+    localStorage.removeItem("previousSearch");
+    previousSearchEl.innerHTML = "";
+  });
 
   
 
@@ -137,17 +207,29 @@ searchEl.addEventListener('submit', function(event) {
  
     fetch(requestUrl)
       .then(function (response) {
-        return response.json();
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            return false;
+        }
       })
       .then(function (data) {
-        console.log(data);
-        var lat = data.coord.lat;
-        var lon = data.coord.lon;
-        cityEl.textContent = data.name;
-
-
-        weatherFetch2(oneCallURL + lat + "&lon=" + lon + apiKey);
-
+        if (data) {
+            console.log(data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            cityEl.textContent = data.name;
+            coordinatesEl.textContent = lat + ", " + lon;
+            weatherFetch2(oneCallURL + lat + "&lon=" + lon + apiKey);
+            localStorage.setItem("previousSearch", JSON.stringify(previousSearchArray) );
+        } else { // error handling
+            cityEl.textContent = "Error - Invalid City";
+            previousSearchArray.splice(0, 1);
+            previousSearchEl.removeChild(previousSearchEl.firstChild);
+            setTimeout(function () {
+                cityEl.textContent = previousSearchArray[0];
+            },1000);
+        }
       });
   }
   function weatherFetch2(requestUrl) {
@@ -160,52 +242,57 @@ searchEl.addEventListener('submit', function(event) {
             console.log(data);
             console.log();
             let uvIndex = data.current.uvi;
+            let night = (data.current.weather[0].icon[2] == 'n');
+
            
-            uvIndexEl.textContent = uvIndex;
+            weatherEl.textContent = data.current.weather[0].main;
+            if (night) {
+                weatherIconEl.innerHTML = iconsNight[data.current.weather[0].id];
+            } else {
+                weatherIconEl.innerHTML = iconsDay[data.current.weather[0].id];
+            }
+            
+
             temperatureEl.textContent = Math.round( data.current.temp ) + "°F";
             humidityEl.textContent = data.current.humidity + "%";
             windSpeedEl.textContent = Math.round( data.current.wind_speed ) + " mph";
+            uvIndexEl.textContent = uvIndex;
 
             switch (uvIndex) {
                 case 0:
                 case 1:
                 case 2: {
-                    uvIndexContainerEl.style.backgroundColor = "#00ff00dd";
+                    uvIndexEl.style.backgroundColor = "#00ff0088";
                     break;
                 }
                 case 3:
                 case 4:
                 case 5: {
-                    uvIndexContainerEl.style.backgroundColor = "#ffff0088";
+                    uvIndexEl.style.backgroundColor = "#ffff0088";
                     break;
                 }
                 case 6:
                 case 7: {
-                    uvIndexContainerEl.style.backgroundColor = "#ff990088";
+                    uvIndexEl.style.backgroundColor = "#ff990088";
                     break;
                 }
                 case 8: 
                 case 9: 
                 case 10: {
-                    uvIndexContainerEl.style.backgroundColor = "#ff000088";
+                    uvIndexEl.style.backgroundColor = "#ff000088";
                     break;
                 }
                 default: {
-                    uvIndexContainerEl.style.backgroundColor = "#ff00ff88";
+                    uvIndexEl.style.backgroundColor = "#ff00ff88";
                     
                 }
 
             }
        
           
-            forecastEl.children[0].children[0].children[0].innerHTML = "Tomorrow";
-            forecastEl.children[1].children[0].children[0].innerHTML = date.add(2,'days').format('dddd');
-            forecastEl.children[2].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
-            forecastEl.children[3].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
-            forecastEl.children[4].children[0].children[0].innerHTML = date.add(1,'days').format('dddd');
     
             for (var i=0; i < 5; i++) {
-                forecastEl.children[i].children[0].children[1].innerHTML = icons[data.hourly[i+1].weather[0].id];
+                forecastEl.children[i].children[0].children[1].innerHTML = iconsDay[data.hourly[i+1].weather[0].id];
                 forecastEl.children[i].children[0].children[2].innerHTML = Math.round( data.hourly[i+1].temp ) + "°F";
             }
 
